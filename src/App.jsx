c489005e3334a86009ce412d92dee2512b22c136
Khushal Kumar <khushalbhatt17@gmail.com>
2025-03-03 19:49:51 +0530

    Tailwind first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      <div style={{backgroundColor:"Red"}}>Hi</div>
+      <div style={{backgroundColor:"Green"}}>Hello</div>
+      <div style={{backgroundColor:"Blue"}}>Hey</div>
+    </div>
+    <div className='flex justify-between'>
+      <div className='bg-red-500'>Hi</div>
+      <div className='bg-green-500'>Hello</div>
+      <div className='bg-blue-500'>Hey</div>
+    </div>
+    </>
+  )
+}
+
+export default App
