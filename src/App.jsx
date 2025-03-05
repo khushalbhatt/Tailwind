fba733a5efee32d6b0410838ae18c85b8a0f69ba
Khushal Kumar <khushalbhatt17@gmail.com>
2025-03-05 23:36:59 +0530

    flex and grid

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,20 @@ function App() {
       <div className='bg-blue-500'>Hey</div>
       <div className='bg-blue-500'>Hey</div>
     </div>
+    <br></br>
+    <br></br>
+    <br></br>
+
+    {/* GRID */}
+    <div className='grid grid-cols-12 gap-4'>
+      <div className='bg-red-500 col-span-5'>Hi</div>
+      <div className='bg-green-500 col-span-5'>Hello</div>
+      <div className='bg-blue-500 col-span-2'>Hey</div>
+    </div>
+     <br></br>
+     <br></br>
+    <br></br>
+    <br></br>
     </>
   )
 }
