40fc994563fda5d2c79a2851a7876a3e8a823fd7
Khushal Kumar <khushalbhatt17@gmail.com>
2025-03-06 00:05:27 +0530

    responsiveness code

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,12 @@ function App() {
      <br></br>
     <br></br>
     <br></br>
+
+     <div className='grid grid-cols-1 md:grid-cols-3 '>
+     <div className='bg-red-500'>Hi</div>
+      <div className='bg-green-500'>Hello</div>
+      <div className='bg-blue-500'>Hey</div>
+     </div>
     </>
   )
 }
